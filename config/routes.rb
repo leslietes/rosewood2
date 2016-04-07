78140c7e1a5baed8178fa1b9637655639b9fa48c
Leslie <leslie.tanengsy@gmail.com>
2016-04-07 15:35:15 +1000

    Add application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resource :setting
+  end
+  
+  get 'home/about'
+  get 'home/contact'
+  get 'home/location'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
