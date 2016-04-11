e4e1098494be65f0b57d1275d8976330b559c2d5
Leslie <leslie.tanengsy@gmail.com>
2016-04-12 09:10:49 +1000

    Add Setting module without admin namespace. Remove logo upload.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_filter :get_settings
-  
   protected
   
-  def get_settings
-    @setting = Setting.first || Setting.new
-  end
-  
   def after_sign_in_path_for(resource)
-    admin_setting_url
+    admin_url
   end
 end
