9c33621cf9c57c6115ec32210aa24fb5dba8e52b
Leslie <leslie.tanengsy@gmail.com>
2016-04-12 13:03:34 +1000

    Add validation to rooms and minor fixes

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ActiveRecord::Base
+  validates :room_no,       presence: true
+  validates :max_occupants, presence: true, numericality: true
+  validates :room_rate,     presence: true, numericality: true
 end
