0af7c316dbafa66e534f3f750c1b0c2bb5485992
Leslie <leslie.tanengsy@gmail.com>
2016-05-02 19:40:06 +1000

    Edit js for flexslider

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,13 @@
-﻿$(document).ready(function(){
+﻿$(window).load(function () {
+    $('.flexslider').flexslider();
+});
+
+
+$(document).ready(function(){
   $('#occupants_list').dataTable();
   $('#rooms_list').dataTable();
   $('#utilities_list').dataTable();
   $('#billings').dataTable();
-  $('.flexslider').flexslider();
 
   $('#datepicker').datepicker({
     changeMonth: true,
@@ -13,5 +17,20 @@
     changeMonth: true,
     changeYear: true
   });
-  
 }); 
+
+$(function () {
+    $('#work').carouFredSel({
+        width: '100%',
+        scroll: 1,
+        auto: false,
+        pagination: false,
+        prev: '.prev_item',
+        next: '.next_item'
+    });
+
+    $("#work").touchwipe({
+        wipeLeft: function () { $('.next_item').trigger('click'); },
+        wipeRight: function () { $('.prev_item').trigger('click'); }
+    });
+});
